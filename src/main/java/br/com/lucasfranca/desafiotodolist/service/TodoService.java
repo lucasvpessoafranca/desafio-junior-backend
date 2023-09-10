6afdba1e0abede37fd96e0b85c17fe495bcc7a49
Lucas França <lucasvpessoafranca@gmail.com>
2023-09-10 13:42:00 -0300

    feat: add  create, update, delete method

--- src/main/java/br/com/lucasfranca/desafiotodolist/service/TodoService.java
+++ src/main/java/br/com/lucasfranca/desafiotodolist/service/TodoService.java
@@ -17,9 +17,10 @@ public class TodoService {
         this.todoRepository = todoRepository;
     }
 
-    // public List<Todo> create(Todo todo) {
-    // todoRepository.save(todo);
-    // }
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
 
     public List<Todo> list() {
         Sort sort = Sort.by("prioridade").descending().and(
@@ -29,12 +30,15 @@ public class TodoService {
 
     }
 
-    // public List<Todo> update() {
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
 
-    // }
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
 
-    // public List<Todo> delete() {
-
-    // }
+    }
 
 }
