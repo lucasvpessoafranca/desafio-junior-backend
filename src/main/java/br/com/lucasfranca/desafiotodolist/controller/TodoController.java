3057fd1b156f35bbf2065c47f82dabc5839bb040
Lucas França <lucasvpessoafranca@gmail.com>
2023-09-10 14:32:28 -0300

    feat: add method list, updat, delete in controller

--- src/main/java/br/com/lucasfranca/desafiotodolist/controller/TodoController.java
+++ src/main/java/br/com/lucasfranca/desafiotodolist/controller/TodoController.java
@@ -2,6 +2,12 @@ package br.com.lucasfranca.desafiotodolist.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +24,25 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    List<Todo> create(Todo todo) {
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo) {
         return todoService.create(todo);
 
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
     }
 
 }
